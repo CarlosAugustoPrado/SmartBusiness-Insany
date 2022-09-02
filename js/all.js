019855543c4b977688d04ab346bef15e9648e386
CarlosAugustoPrado <carlosaugusto_prado@yahoo.com.br>
2022-09-02 14:50:09 -0300

    criação da sessão blog

--- js/all.js
+++ js/all.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";var slideBlog=new Swiper(".slide-blog",{slidesPerView:4,pagination:{el:".swiper-pagination",clickable:!0}});
---
+++ js/scripts/main.js
@@ -0,0 +1,7 @@
+var slideBlog = new Swiper(".slide-blog", {
+  slidesPerView: 4,  
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
